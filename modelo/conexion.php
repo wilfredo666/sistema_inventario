8464d3b71931af6f46e29edfc5605c52673b8f06
amaru <amaru.el.trueno@gmail.com>
2023-04-30 18:23:49 -0400

    prueba conexion bd hosting

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -4,10 +4,22 @@ class Conexion
 {
     static public function conectar()
     {
-        $host = "localhost";
+        /* ==============================
+        PARA TRABAJAR DE MANERA LOCAL
+        ============================== */
+
+        /* $host = "localhost";
         $db = "sistema_inventario";
         $userDB = "root";
-        $passDB = "";
+        $passDB = ""; */
+
+        /* ==============================
+        PARA CONCETAR BD CON EL HOSTING
+        ============================== */
+        $host = "localhost";
+        $db = "u263048258_sis_inventario";
+        $userDB = "u263048258_sis_inventario";
+        $passDB = "Administrador123";
 
         $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
         $link->exec("set names utf8");
