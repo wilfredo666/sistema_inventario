9bcf454937efd1ec82314678031a2a1ccb6cd4e8
wilfredo666 <31371350+wilfredo666@users.noreply.github.com>
2023-07-25 19:00:53 -0400

    conexion host

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -8,18 +8,18 @@ class Conexion
         PARA TRABAJAR DE MANERA LOCAL
         ============================== */
 
-        $host = "localhost";
+        /*$host = "localhost";
         $db = "sistema_inventario";
         $userDB = "root";
-        $passDB = "";
+        $passDB = "";*/
 
         /* ==============================
         PARA CONCETAR BD CON EL HOSTING
         ============================== */
-        /* $host = "localhost";
-        $db = "u263048258_sis_inventario";
-        $userDB = "u263048258_sis_inventario";
-        $passDB = "Administrador123"; */
+         $host = "localhost";
+        $db = "u184609663_inventario";
+        $userDB = "u184609663_root";
+        $passDB = "Admin123!"; 
 
         $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
         $link->exec("set names utf8");
