524e50074efe6ca4535f01dc6604dd2291d03715
wilfredo666 <31371350+wilfredo666@users.noreply.github.com>
2023-07-25 18:28:24 -0400

    7777

--- vista/login.php
+++ vista/login.php
@@ -1,7 +1,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="#"><b>Sistema</b>HERMEN</a>
+            <a href="#"><b>Sistema</b>HER</a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
