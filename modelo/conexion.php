973cb7a35afcd6add3366eddee9f5bac249c3656
EliseoWil <109935327+EliseoWil@users.noreply.github.com>
2025-01-06 23:12:24 -0400

    prueba de conexion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -8,20 +8,26 @@ class Conexion
     /* =============================
          PARA TRABAJAR DE MANERA LOCAL 
          =================================*/
-   /* $host = "localhost";
+
+    /* $host = "localhost";
     $db = "sistema_inventario";
     $userDB = "root";
-    $passDB = "";*/
+    $passDB = "";
+    
+    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB); */
 
-    /* =============================
-         PARA CONECTAR CON EL HOSTING
+    /* ====================================
+         PARA CONECTAR CON EL PUERTO 3307
          =================================*/
     $host = "localhost";
-    $db = "u184609663_inventario";
-    $userDB = "u184609663_root";
-    $passDB = "Admin123!";
+    $db = "sistema_inventario";
+    $userDB = "root";
+    $passDB = "";
+    $port = "3307";
 
-    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
+    $link = new PDO("mysql:host=" . $host . ";port=$port;" . "dbname=" . $db, $userDB, $passDB);
+
+
     $link->exec("set names utf8");
     return $link;
   }
