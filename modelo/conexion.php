b20dce77de1421fd92b70e1f3a1046299079beaa
wilfredo666 <u184609663@us-phx-web628.main-hosting.eu>
2025-01-23 12:14:45 +0000

    conexion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -27,8 +27,19 @@ class Conexion
 
     $link = new PDO("mysql:host=" . $host . ";port=$port;" . "dbname=" . $db, $userDB, $passDB); */
 
+    /* =============================
+         PARA TRABAJAR CON EL SERVIDOR REMOTO
+         =================================*/
+    /* 
+    $host = "localhost";
+    $db = "u184609663_inventario";
+    $userDB = "u184609663_root";
+    $passDB = "Admin123!";
+    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
+    */
 
     $link->exec("set names utf8");
     return $link;
   }
 }
+
