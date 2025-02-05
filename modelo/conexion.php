d74bc66cf9b8dbc6ea62b36ae5ae9d9fc45b951a
wilfredo666 <31371350+wilfredo666@users.noreply.github.com>
2025-02-05 16:18:20 -0400

    test 3

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -9,33 +9,22 @@ class Conexion
          PARA TRABAJAR DE MANERA LOCAL 
          =================================*/
 
-/*    $host = "localhost";
+    $host = "localhost";
     $db = "sistema_inventario";
     $userDB = "root";
     $passDB = "";
 
-    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);*/
-
-    /* ====================================
-         PARA CONECTAR CON EL PUERTO 3307
-         =================================*/
-    /* $host = "localhost";
-    $db = "sistema_inventario";
-    $userDB = "root";
-    $passDB = "";
-    $port = "3307";
-
-    $link = new PDO("mysql:host=" . $host . ";port=$port;" . "dbname=" . $db, $userDB, $passDB); */
+    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
 
     /* =============================
       PARA TRABAJAR CON EL SERVIDOR REMOTO
       =================================*/
      
-    $host = "localhost";
+/*    $host = "localhost";
     $db = "u184609663_inventario";
     $userDB = "u184609663_root";
     $passDB = "Admin123!";
-    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);
+    $link = new PDO("mysql:host=" . $host . ";" . "dbname=" . $db, $userDB, $passDB);*/
     
 
     $link->exec("set names utf8");
